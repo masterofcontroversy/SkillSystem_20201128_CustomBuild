e8ba96602d4f749d32383a26ba038853413596d6
masterofcontroversy <masterofcontroversy@mail.com>
2021-11-10 14:36:30 -0500

    Added windows script for building dependencies

---
+++ MakeDeps.cmd
@@ -0,0 +1,60 @@
+@echo off
+
+@rem USAGE: "MAKE HACK_full.cmd" [quick]
+@rem If first argument is "quick", then this will not update text, tables, maps, or generate a patch
+@rem "MACK HACK_quick.cmd" simply calls this but with the quick argument, for convenience
+
+@rem defining buildfile config
+
+set "source_rom=%~dp0FE8_clean.gba"
+
+set "main_event=%~dp0ROM Buildfile.event"
+
+set "target_rom=%~dp0SkillsTest.gba"
+set "target_ups=%~dp0SkillsTest.ups"
+
+@rem defining tools
+
+set "c2ea=%~dp0Tools\C2EA\c2ea"
+set "textprocess=%~dp0Tools\TextProcess\text-process-classic"
+set "ups=%~dp0Tools\ups\ups"
+set "parsefile=%~dp0Event Assembler\Tools\ParseFile.exe"
+set "tmx2ea=%~dp0Tools\tmx2ea\tmx2ea"
+
+@rem set %~dp0 into a variable because batch is stupid and messes with it when using conditionals?
+
+set "base_dir=%~dp0"
+
+@rem do the actual building
+
+if /I [%1]==[assemble] (
+  echo Copying ROM
+  copy "%source_rom%" "%target_rom%"
+)
+
+  @rem only do the following if this isn't a make hack quick
+
+  echo:
+  echo Processing tables
+
+  cd "%base_dir%Tables"
+  echo: | ("%c2ea%" "%source_rom%" -installer "%base_dir%Tables/TableInstaller.event")
+
+  echo:
+  echo Processing text
+
+  cd "%base_dir%Text"
+  echo: | ("%textprocess%" text_buildfile.txt --parser-exe "%parsefile%" --installer "InstallTextData.event" --definitions "TextDefinitions.event")
+
+if /I [%1]==[assemble] (
+echo:
+echo Assembling
+
+cd "%base_dir%Event Assembler"
+ColorzCore A FE8 "-output:%target_rom%" "-input:%main_event%"
+)
+
+echo:
+echo Done!
+
+pause
